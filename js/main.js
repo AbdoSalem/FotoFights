c68fb3fc2b1de0f404201ecdba42b673f8de36e1
Koding <agwatic@agwatic.koding.io>
2016-02-20 15:31:39 -0600

    adds JSON parser for the result of the request and returns the parameters used in the comparison

--- js/main.js
+++ js/main.js
@@ -47,6 +47,27 @@ $(function () {
     }
 }
 
+    //use this function in done event with the input data with no change
+    function parseJSON(imgData){
+        
+    	var dataLength = imgData.length;
+    	var happiness = 0;
+    	var anger = 0;
+    	var fear = 0;
+    	var surprise = 0;
+    	for(var i =0;i<dataLength;i++){
+    		happiness += imgData[i]["scores"]["happiness"];
+    		anger += imgData[i]["scores"]["anger"];
+    		fear += imgData[i]["scores"]["fear"];
+    		surprise += imgData[i]["scores"]["surprise"];
+
+    	}
+    	//the return data are happiness , anger , fear and surprise respectively
+        //use it in filling the bars under the images
+    	params_arr = [(happiness/dataLength), (anger/dataLength), (fear/dataLength), (surprise/dataLength)];
+    	return params_arr;
+
+    }
 $("#input_file_left").change("#img_left",readImage);
 $("#input_file_right").change("#img_right",readImage );
 
